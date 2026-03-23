3869765ae1bb3588b5e6baeff31cbac543d213f6
tayyabofficial089-coder <tayyabofficial089@gmail.com>
2026-03-23 13:40:00 +0000

    Add Admin database model

---
+++ backend/src/models/Admin.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Admin = sequelize.define('Admin', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'users',
+            key: 'id'
+        }
+    }
+}, {
+    timestamps: true,
+    tableName: 'admins'
+});
+
+module.exports = Admin;
